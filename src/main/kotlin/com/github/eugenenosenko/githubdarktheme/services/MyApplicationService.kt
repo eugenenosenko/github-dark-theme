580a1a19c515e0c38c52359a7ebde5aa8ae179a5
GitHub Action <action@github.com>
2020-12-24 16:16:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/eugenenosenko/githubdarktheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.eugenenosenko.githubdarktheme.services
+
+import com.github.eugenenosenko.githubdarktheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
